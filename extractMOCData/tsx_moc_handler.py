72f1cb2f2a327d518a68300166669b9ea5da0dd4
gryBox <livni.itay@gmail.com>
2020-04-03 20:16:54 -0500

    finished writing lambda handler

--- extractMOCData/tsx_moc_handler.py
+++ extractMOCData/tsx_moc_handler.py
@@ -10,10 +10,10 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     
           
-        url = event.get("url", cfg.TSX_MOC_DATA 'https://api.tmxmoney.com/mocimbalance/en/TSX/moc.html')
-        flnm = event.get("flnm", cfg.TSX_MOC_DATA"moc_tsx")
-        put_dir= event.get("put_dir", cfg.TSX_MOC_DATA"s3://tsx-moc/")
-        fillna_clmn = event.get("fillna_clmn", cfg.TSX_MOC_DATA"Symbol")
+        url = event.get("url", cfg.TSX_MOC_DATA["EXTRACT_URL"])
+        flnm = event.get("flnm", cfg.TSX_MOC_DATA["FLNAME"])
+        put_dir= event.get("put_dir", cfg.TSX_MOC_DATA["MOC_DATA_FLPTH"])
+        fillna_clmn = event.get("fillna_clmn", cfg.TSX_MOC_DATA["FILL_NA_CLMN"])
 
         # Scrape amd dump MOC data to s3 bucket
         tsxMocData = TsxMocData(
@@ -23,5 +23,8 @@ def lambda_handler(event, context):
         moc_df = tsxMocData.scrape_moc_data()
 
         # Write
+        put_flpth = tsxMocData.write_tsx_moc(moc_df)
 
-    return json.loads(resp.read().decode())
+    return {
+        "put_flpth": put_flpth
+    }
