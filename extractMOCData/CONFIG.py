2b8fe119052cd6a0ca31e6b1fc9d67facd7a777a
gryBox <livni.itay@gmail.com>
2020-04-29 12:21:59 -0500

    deleted files not pertaining to guide

--- extractMOCData/CONFIG.py
+++
@@ -1,7 +0,0 @@
-TSX_MOC_DATA = {
-    "MOC_DATA_FLPTH":"s3://tsx-moc/",
-    "FLNAME": "moc_tsx",
-    "EXTRACT_URL": 'https://api.tmxmoney.com/mocimbalance/en/TSX/moc.html',
-    "FILL_NA_CLMN": "Symbol"
-}
-
