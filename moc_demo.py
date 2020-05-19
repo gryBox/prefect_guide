730a1bd192863cf0528995bcff0f94c2c9f886ae
gryBox <livni.itay@gmail.com>
2020-05-19 11:24:44 -0500

    added results

--- moc_demo.py
+++ moc_demo.py
@@ -4,7 +4,7 @@ import sqlalchemy as sa
 from datetime import timedelta
 
 from prefect import task, Flow, Parameter, unmapped
-from prefect.engine.result_handlers import LocalResultHandler, S3ResultHandler
+from prefect.engine.results import LocalResult
 from prefect.engine.state import Success, Failed, Skipped
 
 from prefect.client.secrets import Secret
@@ -14,8 +14,8 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3_handler = S3ResultHandler(bucket='tsx-moc-bcp')  
-lcl_handler = LocalResultHandler()
+# s3_handler = S3ResultHandler(bucket='tsx-moc-bcp')  
+# lcl_handler = LocalResultHandler()
 
 def error_notifcation_handler(obj, old_state, new_state):
     # Hamdle an empty dataframe to return a fail message.  
@@ -51,8 +51,9 @@ def imb_handler(obj, old_state, new_state):
 @task(
     max_retries=2, 
     retry_delay=timedelta(seconds=1),
-    result_handler=s3_handler,
-    state_handlers=[imb_handler, error_notifcation_handler])
+    result=LocalResult(location="~/prefect_guide/results/{date}/{task_name}.prefect")
+    #state_handlers=[imb_handler, error_notifcation_handler]
+    )
 def get_tsx_moc_imb(url: str):
     """
     Scrape the TSX website Market on close website. Data only available weekdays after 15:40 pm Toronto time
@@ -61,6 +62,8 @@ def get_tsx_moc_imb(url: str):
     Use archived url for testing.       
     "https://web.archive.org/web/20200414202757/https://api.tmxmoney.com/mocimbalance/en/TSX/moc.html"
     """
+
+    #raise Exception
     
     # 1, Get the html content
     html = requests.get(url).content
@@ -76,12 +79,13 @@ def get_tsx_moc_imb(url: str):
 
 @task
 def partition_df(df, n_conn=1):
-    return np.array_split(df, n_conn)
+    df_lst = np.array_split(df, n_conn)
+    return df_lst
 
 
 @task
 def df_to_db(df, tbl_name, conn_str):
-
+    raise Exception
     engine = sa.create_engine(conn_str)
     
     # Changer "if_exist" to "append" when done devolpment
@@ -92,17 +96,20 @@ def df_to_db(df, tbl_name, conn_str):
     return df.shape
 
 
-
+# A flow has no particular order unless the data is binded (shown) or explicitly set (not shown).
 with Flow(name="Get-TSX-MOC-Imbalances") as tsx_imb_fl:
     
     tsx_url = Parameter("tsx_url", default="https://api.tmxmoney.com/mocimbalance/en/TSX/moc.html")
-    imb_tbl_nm = Parameter("imb_tbl_nm", default="moc_tst") 
+    imb_tbl_nm = Parameter("imb_tbl_nm", default="moc_tst")
+    n_conn = Parameter("n_conn", default=1) 
     
     tsx_imb_df = get_tsx_moc_imb(tsx_url)
 
     conn_str = PrefectSecret("moc_pgdb_conn")
     
-    df_shape = df_to_db.map(tsx_imb_df, tbl_name=unmapped(imb_tbl_nm), conn_str=unmapped(conn_str))
+    tsx_imb_df_lst = partition_df(tsx_imb_df, n_conn)
+
+    df_shape = df_to_db.map(tsx_imb_df_lst, tbl_name=unmapped(imb_tbl_nm), conn_str=unmapped(conn_str))
 
 if __name__ == "__main__":
 
